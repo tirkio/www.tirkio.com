2ab17a34d4299ce48d3536b444ef849ea3c86ef8
Flavio Aiello <flavioaiello@colosseo.home>
2024-12-21 09:22:08 +0100

    update

--- fix.sh
+++ fix.sh
@@ -11,8 +11,25 @@ backup_file() {
 
 # Remove custom includes
 if [ -d "_includes" ]; then
-  echo "Removing custom includes..."
-  rm -rf _includes
+  echo "Restoring custom includes..."
+  mkdir -p _includes
+  cat <<EOL > _includes/disqus.html
+<section class="disqus">
+    <div id="disqus_thread"></div>
+    <script type="text/javascript">
+        var disqus_shortname = '{{site.disqus}}'; 
+        var disqus_developer = 0;
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = window.location.protocol + '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+    <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+</section>
+EOL
+  echo "Disqus include restored in _includes/disqus.html."
 fi
 
 # Restore navigation in `_layouts/default.html`
@@ -28,17 +45,21 @@ sed -i '' "/<div class='collapse navbar-collapse'/a \
 
 echo "Navigation reset to default in $layout_file."
 
-# Remove unnecessary Disqus integration
-includes_disqus="_includes/disqus.html"
-if [ -f "$includes_disqus" ]; then
-  echo "Removing Disqus integration..."
-  rm "$includes_disqus"
-fi
-
+# Re-include Disqus in `_layouts/post.html`
 default_post_layout="_layouts/post.html"
 backup_file "$default_post_layout"
-sed -i '' '/{%-? include disqus.html -?%}/d' "$default_post_layout"
-echo "Disqus references removed from $default_post_layout."
+sed -i '' "/<!-- Begin Comments/,/<!--End Comments/d" "$default_post_layout"
+sed -i '' "/<div class='article-post serif-font'>/a \
+            <!-- Begin Comments\
+            ================================================== -->\
+            <section>\
+            <div id="comments">\
+                {% include disqus.html %} \n            </div>\
+            </section>\
+            <!--End Comments\
+            ================================================== -->" "$default_post_layout"
+
+echo "Disqus integration re-included in $default_post_layout."
 
 # Reset `_config.yml` to default settings
 config_file="_config.yml"
@@ -50,15 +71,16 @@ timezone: UTC
 baseurl: ""
 permalink: "/:title/"
 
+disqus: "your-disqus-shortname"
+
 plugins:
   - jekyll-feed
   - jekyll-sitemap
   - jekyll-seo-tag
 
-disqus: "" # Removed
 paginate: 10
 EOL
-echo "_config.yml reset to default."
+echo "_config.yml reset to default with Disqus configuration."
 
 # Clean custom layouts
 for layout in about contact categories privacy-policy; do
@@ -69,5 +91,4 @@ for layout in about contact categories privacy-policy; do
 
 done
 
-echo "Customizations removed. Run 'jekyll serve' to test your site."
-
+echo "Customizations removed and Disqus re-included. Run 'jekyll serve' to test your site."
